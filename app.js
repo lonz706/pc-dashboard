0a35dfdd79181fd2c9b09c435a5eda3cb4567ae2
Lorenz <lorenz-hohmann@online.de>
2017-08-28 19:58:56 +0200

    added comments

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var express 	= require('express'),
 	os			= require('os'),
 	io			= require('socket.io');
 
-// view engine (ejs)
+
+// setup view engine (ejs)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-// set static path for assets
+// set static path for assets (*.css, *.js, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -25,20 +26,20 @@ server.listen(3000);
 console.log('Server running...');
 
 
-// handle server requests
+// handle tcp-server requests
 io.on('connection', function(socket) {
 
 	// handle update socket request
 	socket.on('update dashboard', function() {
 
-		var freeMemory  		= os.freemem(),
-			totalMemory 		= os.totalmem(),
-			usedMemory			= totalMemory - freeMemory,
-			usedMemoryRounded	= Math.round(((totalMemory - freeMemory) / 1024 / 1024 / 1024) * 10) / 10;
-			freeMemoryRounded	= Math.round((freeMemory / 1024 / 1024 / 1024) * 10) / 10,			// bytes to gebibytes
-			totalMemoryRound	= Math.round((totalMemory / 1024 / 1024 / 1024) * 10) / 10,			// bytes to gebibytes
-			usedMemoryInPercent = Math.round((100 * usedMemory / totalMemory) * 10) / 10,
-			usedMemoryInPercent = (usedMemoryInPercent>100?100:usedMemoryInPercent);
+		var freeMemory  		= os.freemem(),																// free memory in bytes
+			totalMemory 		= os.totalmem(),															// total memory in bytes
+			usedMemory			= totalMemory - freeMemory,													// total memory calculated by free- and total memoey
+			usedMemoryRounded	= Math.round(((totalMemory - freeMemory) / 1024 / 1024 / 1024) * 10) / 10;	// used memory rounded with two decimal numbers
+			freeMemoryRounded	= Math.round((freeMemory / 1024 / 1024 / 1024) * 10) / 10,					// round free memory in gebibytes with two decimal numbers
+			totalMemoryRound	= Math.round((totalMemory / 1024 / 1024 / 1024) * 10) / 10,					// round used memory in gebibytes with two decimal numbers
+			usedMemoryInPercent = Math.round((100 * usedMemory / totalMemory) * 10) / 10,					// calculate used memory in percent with two decimal numbers
+			usedMemoryInPercent = (usedMemoryInPercent>100?100:usedMemoryInPercent);						// if usedMemory > 100 = set to 100 to prevent view-errors
 
 		var data = {
 			used_memory: usedMemoryRounded,
@@ -48,11 +49,11 @@ io.on('connection', function(socket) {
 
 		socket.emit('update frontend', data);
 	});
+
 });
 
 
 // catch /-request
 app.get('/', function(req, res) {
 	res.render('index');
-	// socket.emit('update dashboard');
 });
